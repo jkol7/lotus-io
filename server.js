3fbae8210864fc88dd2ef482241d464fe25ca935
jkol7 <jkolcun7@gmail.com>
2024-09-07 13:49:47 -0400

    Before deployment code added

--- server.js
+++ server.js
@@ -1,37 +1,39 @@
+require("dotenv").config();
+const path = require("path");
+const express = require("express");
+const axios = require("axios");
+const cors = require("cors");
 
+const app = express();
 
-require('dotenv').config()
-const path = require('path');
-const express = require('express')
-const axios = require('axios')
-const cors = require('cors');
+app.use(express.static("client/build"));
 
-
-const app = express()
-
-app.use(express.static('client/build'))
-
-app.get('/', (req, res)=> {
-    res.send('Working main directory')
+/*
+app.get("/", (req, res) => {
+  res.send("Working main directory");
 });
 
-app.get('/youtube', async (req, res) => {
+*/
 
-    const pose = req.query.id + " pose"
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
 
-    const data = await axios ( {
-        
-        url: `https://youtube-search-and-download.p.rapidapi.com/search?query=${pose}`,
-        headers: {
-            'X-RapidAPI-Key': process.env.RAPID_API_KEY,
-            'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com'
-          }
-    } )
-    res.send(data.data.contents)
-})
+app.get("/youtube", async (req, res) => {
+  const pose = req.query.id + " pose";
 
+  const data = await axios({
+    url: `https://youtube-search-and-download.p.rapidapi.com/search?query=${pose}`,
+    headers: {
+      "X-RapidAPI-Key": process.env.RAPID_API_KEY,
+      "X-RapidAPI-Host": "youtube-search-and-download.p.rapidapi.com",
+    },
+  });
+  res.send(data.data.contents);
+});
 
-  
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
